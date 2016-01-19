73777411104e261fa5b7722587c560a42f1c14a2
James Keener <jimmybobkeenmachine@gmail.com>
2016-01-19 14:25:32 -0700

    Init
    site

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        anchors: ['home', 'skills', 'contact'],
+        onLeave: function (index, nextIndex, direction) {
+            var leavingSection = $(this);
+
+            //after leaving section 2
+            if (index === 1 && nextIndex === 2) {
+                $(".main-nav-heading").velocity("transition.flipYIn").text("sKILLS");
+            } else if (index === 1 && nextIndex === 3) {
+                $(".main-nav-heading").velocity("transition.flipYIn").text("cONTACT");
+            } else if (index === 2 && nextIndex === 1) {
+                $(".main-nav-heading").velocity("transition.flipYIn").text("jKEENER");
+            } else if (index === 2 && nextIndex === 3) {
+                $(".main-nav-heading").velocity("transition.flipYIn").text("cONTACT");
+            } else if (index === 3 && nextIndex === 1) {
+                $(".main-nav-heading").velocity("transition.flipYIn").text("jKEENER");
+            } else if (index === 3 && nextIndex === 2) {
+                $(".main-nav-heading").velocity("transition.flipYIn").text("sKILLS");
+            }
+        }
+    });
+    $(".small-nav-heading").click(function () {
+        $(".small-nav-heading").velocity("transition.flipYOut", {
+            duration: 300
+        });
+        $(".small-nav-options").velocity("transition.flipYIn", {
+            duration: 200,
+            delay: 300
+        });
+    });
+    $(".small-nav-options").click(function () {
+        $(".small-nav-options").velocity("transition.flipYOut", {
+            duration: 300
+        });
+        $(".small-nav-heading").velocity("transition.flipYIn", {
+            duration: 200,
+            delay: 300
+        });
+    });
+    
+    $(".squiggle-triangles").velocity({opacity: 1}, {delay: 4000});
+});
+
+var path = document.querySelector('.squiggle');
+var length = path.getTotalLength();
+console.log(length);

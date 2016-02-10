65f8115bdf4b77f299fd59d22ba5a8cb3e86f746
James Keener <jimmybobkeenmachine@gmail.com>
2016-02-10 10:40:08 -0700

    Animation
    Changed the line animation functions to work better.

--- js/main.js
+++ js/main.js
@@ -1,59 +1,70 @@
 $(document).ready(function () {
-    $('#fullpage').fullpage({
-        anchors: ['home', 'skills', 'contact'],
-        onLeave: function (index, nextIndex, direction) {
-            var leavingSection = $(this);
+            $('#fullpage').fullpage({
+                anchors: ['home', 'skills', 'contact'],
+                onLeave: function (index, nextIndex, direction) {
+                    var leavingSection = $(this);
 
-            //after leaving section 2
-            if (index === 1 && nextIndex === 2) {
-                skillsAnimate();
-                $(".main-nav-heading").velocity("transition.flipYIn").text("sKILLS");
-            } else if (index === 1 && nextIndex === 3) {
-                $(".main-nav-heading").velocity("transition.flipYIn").text("cONTACT");
-            } else if (index === 2 && nextIndex === 1) {
-                $(".main-nav-heading").velocity("transition.flipYIn").text("jKEENER");
-            } else if (index === 2 && nextIndex === 3) {
-                $(".main-nav-heading").velocity("transition.flipYIn").text("cONTACT");
-            } else if (index === 3 && nextIndex === 1) {
-                $(".main-nav-heading").velocity("transition.flipYIn").text("jKEENER");
-            } else if (index === 3 && nextIndex === 2) {
-                skillsAnimate();
-                $(".main-nav-heading").velocity("transition.flipYIn").text("sKILLS");
+                    //after leaving section 2
+                    if (index === 1 && nextIndex === 2) {
+                        skillsAnimate();
+                        $(".main-nav-heading").velocity("transition.flipYIn").text("sKILLS");
+                    } else if (index === 1 && nextIndex === 3) {
+                        $(".main-nav-heading").velocity("transition.flipYIn").text("cONTACT");
+                    } else if (index === 2 && nextIndex === 1) {
+                        $(".main-nav-heading").velocity("transition.flipYIn").text("jKEENER");
+                    } else if (index === 2 && nextIndex === 3) {
+                        $(".main-nav-heading").velocity("transition.flipYIn").text("cONTACT");
+                    } else if (index === 3 && nextIndex === 1) {
+                        $(".main-nav-heading").velocity("transition.flipYIn").text("jKEENER");
+                    } else if (index === 3 && nextIndex === 2) {
+                        skillsAnimate();
+                        $(".main-nav-heading").velocity("transition.flipYIn").text("sKILLS");
+                    }
+                }
+            });
+            $(".small-nav-heading").click(function () {
+                $(".small-nav-heading").velocity("transition.flipYOut", {
+                    duration: 300
+                });
+                $(".small-nav-options").velocity("transition.flipYIn", {
+                    duration: 200,
+                    delay: 300
+                });
+            });
+            $(".small-nav-options").click(function () {
+                $(".small-nav-options").velocity("transition.flipYOut", {
+                    duration: 300
+                });
+                $(".small-nav-heading").velocity("transition.flipYIn", {
+                    duration: 200,
+                    delay: 500
+                });
+            });
+
+            function skillsAnimate() {
+                $(function () {
+                    $(".squiggle").each(function () {
+                        var length = this.getTotalLength();
+                        $(this).css({
+                            "stroke-dasharray": length,
+                            "stroke-dashoffset": length
+                        });
+                    });
+                    $(".squiggle").velocity({
+                        'stroke-dashoffset': 0
+                    }, {
+                        duration: 3000,
+                        delay: 300,
+                        easing: "easeOutSine",
+                        complete: function () {
+                            console.log("done");
+                            $(".squiggle-triangles").velocity({
+                                'opacity': '1'
+                            }, {
+                                duration: 500
+                            });
+                        }
+                    });
+                });
             }
-        }
-    });
-    $(".small-nav-heading").click(function () {
-        $(".small-nav-heading").velocity("transition.flipYOut", {
-            duration: 300
-        });
-        $(".small-nav-options").velocity("transition.flipYIn", {
-            duration: 200,
-            delay: 300
-        });
-    });
-    $(".small-nav-options").click(function () {
-        $(".small-nav-options").velocity("transition.flipYOut", {
-            duration: 300
-        });
-        $(".small-nav-heading").velocity("transition.flipYIn", {
-            duration: 200,
-            delay: 500
-        });
-    });
-
-    function skillsAnimate() {
-
-
-        $('.squiggle').velocity({
-            'stroke-dashoffset': 0
-        }, {
-            duration: 4000,
-            delay: 0
-        });
-        $(".squiggle-triangles").velocity({
-            opacity: 1
-        }, {
-            delay: 4000
-        });
-    }
-});
+            });
